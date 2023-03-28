917153e54a33b39728d146f36373face5dfd6b84
Ms <125511982+mayamelissa15@users.noreply.github.com>
2023-03-28 15:39:26 +0100

    Update

--- test.js
+++
@@ -1,8 +0,0 @@
-document.getElementsById("inscription").addEventlistener('submit', function(e)
-{   e.preventdefault(); // pour eviter le comportement par defaut de la page qui est son rechargement automatique 
-    alert("Formulaire envoyé !");
-}// cet evenement sera declenché automatiquement quand le formulaire sera envoyé 
-
-
-
-)
